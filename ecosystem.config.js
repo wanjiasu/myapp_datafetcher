0dad6d8e21c70c48c7d8bf2a63ccbf29e518005f
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2025-12-07 15:31:17 +0800

    add: pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'tele_agent_fixture',
-      script: 'python',
-      args: '-m uvicorn data_fetcher.api.app:app --host 0.0.0.0 --port 8003',
-      cwd: '.',
-      exec_mode: 'fork',
-      interpreter: 'none',
-      env: {
-        HOST: '0.0.0.0',
-        PORT: '8003'
-      },
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '300M'
+      script: 'env/bin/python',
+      args: '-m uvicorn data_fetcher.api.app:app --host 0.0.0.0 --port 8003'
     }
   ]
 };
